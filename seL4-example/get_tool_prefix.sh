1242e29d4b5d2175bb21b356afc63e8322f5e657
Thomas Sewell <Thomas.Sewell@nicta.com.au>
2016-11-11 15:42:40 +1100

    Report errors sanely in get_tool_prefix.sh

--- seL4-example/get_tool_prefix.sh
+++ seL4-example/get_tool_prefix.sh
@@ -4,5 +4,11 @@ TC=$(find ../.. /opt -maxdepth 2 -name 'toolchain*' 2> /dev/null )
 BIN=$(find / $TC -maxdepth 2 -name 'bin' 2> /dev/null )
 GCC=$(find $BIN -maxdepth 2 -name 'arm-none-eabi-gcc' 2> /dev/null )
 
-ls $GCC | sed 's/-gcc$/-/' | head -n 1
+if [[ -z $GCC ]]
+then
+  echo "No cross compiler found, install arm-none-eabi-gcc?" 1>&2
+else
+  ls $GCC | sed 's/-gcc$/-/' | head -n 1
+fi
+
 
