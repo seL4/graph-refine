aad9bddbe0ea5d39e66e44885f83160d85e6d903
Matthew Brecknell <matt@kry10.com>
2022-03-11 17:52:21 +1100

    cleanup: remove obsolete code, rename misc scripts
    The functionality of some of the removed scripts will be replaced in
    subsequent commits.
    
    Signed-off-by: Matthew Brecknell <matt@kry10.com>

--- graph-to-graph/tests/kernel/wcetx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/ksh
-
-python ../../graph_to_graph.py ./ main --x -a rv64 2>&1 | tee x.log
-#python ../../graph_to_graph.py ./ main --L -a rv64 2>&1 | tee L.log
-
--- graph-to-graph/tests/kernel_arm/wcet.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/ksh
-
-python ../../graph_to_graph.py ./ main --l -a rv64 2>&1 | tee l.log
-python ../../graph_to_graph.py ./ main --L -a rv64 2>&1 | tee L.log
-
